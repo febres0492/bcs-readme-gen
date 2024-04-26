639bc7389dd801701632c9454560a491113f01e5
Carlos Febres <febres0492@gmail.com>
2024-04-26 18:31:02 -0400

    Update index.js to fix formatting and increase file creation limit

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function c(str, color = 'g'){
 
 // TODO: Create an array of questions for user input
 const questions = [
-    { name: 'project_name'    , type: 'input', message: `What is your ${c('Project name')}?`              , title: 'My Project' }     ,
-    { name: 'github_username' , type: 'input', message: `What is your ${c('GitHub','b')} username?`       , title: 'github_username' },
-    { name: 'email_address'   , type: 'input', message: `What is your ${c('Email','y')} address?`         , title: 'email_address' }  ,
+    { name: 'project_name'    , type: 'input', message: `What is your ${c('Project name')}?`              , title: 'My Project' }      , 
+    { name: 'github_username' , type: 'input', message: `What is your ${c('GitHub')} username?`           , title: 'github_username' } , 
+    { name: 'email_address'   , type: 'input', message: `What is your ${c('Email')} address?`             , title: 'email_address' }   , 
     { name: 'usage'           , type: 'list' , message: `Do you need a ${c('Usage')} section?`            , title: 'Usage'             , choices: ['yes', 'no']}  ,
     { name: 'contribution'    , type: 'list' , message: `Do you need a ${c('Contribution')} section?`     , title: 'Contribution'      , choices: ['yes', 'no'] } ,
     { name: 'features'        , type: 'list' , message: `Do you need a ${c('Features')} section?`         , title: 'Features'          , choices: ['yes', 'no'] } ,
@@ -29,16 +29,24 @@ function writeToFile(fileName, data) {
     fileName = 'README.md' 
     let fileCreated = false
     let i = 0
+    let fileAmountLimit = 100
+    let existingFilesIndex = ''
 
     // checing if the file exists and updating the file name
-    while(fileCreated === false && i < 20) {
+    while(fileCreated === false && i < fileAmountLimit) {
+        if(i === fileAmountLimit) { console.log(colors.red('File limit reached. clear the output folder')); break }
         try {
             let subfix = i === 0 ? '' : `_${i}`
             fileName = `README${subfix}.md`
             fs.accessSync(`./output/${fileName}`, fs.F_OK)
-            console.log(colors.yellow(`File ${fileName} already exists.`))
+            existingFilesIndex += `${i},`
         } catch (err) {
-            console.log(ln(),colors.green(`'Creating file...', ${fileName}`))
+            let firstNum = existingFilesIndex.split(',')[0]
+            let lastNum = existingFilesIndex.split(',').at(-1)
+            lastNum = lastNum === '' ? existingFilesIndex.split(',').at(-2) : lastNum
+
+            console.log(colors.yellow(`Files REAME ${firstNum} to ${lastNum} already exists:`), )
+            console.log(ln(),colors.green(`Creating file ${fileName}`))
             fileCreated = true
         }
         i++
@@ -47,7 +55,9 @@ function writeToFile(fileName, data) {
     // generating markdown
     let markdown = ''
     Object.entries(data).forEach(([key, value]) => {
-        if (value === 'yes'){ markdown += generateMarkdown(key) + '\n' }
+        console.log(ln(),key, value)
+
+        // if (value === 'yes'){ markdown += generateMarkdown(key) + '\n' }
     })
 
     // writing to file
@@ -80,7 +90,7 @@ async function init(input = '') {
     if (mode === 'Default') {
         defaultQuestions = [] 
         defaultValues = questions.reduce((acc, question) => {
-            acc[question.name] = 'y' 
+            acc[question.name] = 'yes' 
             return acc
         }, {})
         console.log(ln(),'mode', mode)
